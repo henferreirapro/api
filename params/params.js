035302b7623dd13551a8bb7b2a4c69ed676b6fbc
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2022-10-01 19:51:50 -0300

    Create params.js

---
+++ params/params.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.listen('3000')
+
+app.use(express.json())
+
+app.route('/').get( (req, res) => res.send(req.query.name))
+
+app.route('/').put( (req, res) => res.send(req.query.pessoa))
+
+app.route('/:parametro').get( (req, res) => res.send(req.params.parametro))
+
+app.route('/').post( (req, res) => res.send(req.body))
